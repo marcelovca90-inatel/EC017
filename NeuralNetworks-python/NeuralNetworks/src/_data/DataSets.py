6d98f3a8dd11c7d7cf9fb8df9f832e1122e92f71
Marcelo Cysneiros <marcelovca90@gmail.com>
2020-04-30 13:39:34 -0300

    Remove default parameter value

--- NeuralNetworks-python/NeuralNetworks/src/_data/DataSets.py
+++ NeuralNetworks-python/NeuralNetworks/src/_data/DataSets.py
@@ -6,7 +6,7 @@ from numpy import append, genfromtxt
 class DataSets:
     
     @staticmethod
-    def read(folder, filename, flatten = False):
+    def read(folder, filename, flatten):
         filename_abs = os.path.join(os.path.dirname(__file__), folder, filename)
         return genfromtxt(filename_abs, delimiter=',', dtype=float)
 
@@ -19,30 +19,30 @@ class DataSets:
 
 # https://en.wikipedia.org/wiki/AND_gate
 class LOGIC_GATE_AND:
-    input = DataSets.add_bias(DataSets.read('logic-gate-and', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('logic-gate-and', 'input.txt', False))
     output = DataSets.read('logic-gate-and', 'output.txt', True)
 
 # https://en.wikipedia.org/wiki/OR_gate
 class LOGIC_GATE_OR:
-    input = DataSets.add_bias(DataSets.read('logic-gate-or', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('logic-gate-or', 'input.txt', False))
     output = DataSets.read('logic-gate-or', 'output.txt', True)
 
 # https://en.wikipedia.org/wiki/XOR_gate
 class LOGIC_GATE_XOR:
-    input = DataSets.add_bias(DataSets.read('logic-gate-xor', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('logic-gate-xor', 'input.txt', False))
     output = DataSets.read('logic-gate-xor', 'output.txt', True)
 
 # http://archive.ics.uci.edu/ml/datasets/Blood+Transfusion+Service+Center
 class BLOOD_TRANSFUSION:
-    input = DataSets.add_bias(DataSets.read('blood-transfusion', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('blood-transfusion', 'input.txt', False))
     output = DataSets.read('blood-transfusion', 'output.txt', True)
 
 # https://www.kaggle.com/kumargh/pimaindiansdiabetescsv
 class DIABETES:
-    input = DataSets.add_bias(DataSets.read('diabetes', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('diabetes', 'input.txt', False))
     output = DataSets.read('diabetes', 'output.txt', True)
 
 # https://archive.ics.uci.edu/ml/datasets/Tic-Tac-Toe+Endgame
 class TIC_TAC_TOE_ENDGAME:
-    input = DataSets.add_bias(DataSets.read('tic-tac-toe-endgame', 'input.txt'))
+    input = DataSets.add_bias(DataSets.read('tic-tac-toe-endgame', 'input.txt', False))
     output = DataSets.read('tic-tac-toe-endgame', 'output.txt', True)
